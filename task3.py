6fcea254003d41352ad9992dba19d294047bfccb
Dariusz Zajglic <darzajgl@gmail.com>
2022-11-27 20:25:25 +0100

    task 3 done

--- task3.py
+++ task3.py
@@ -4,3 +4,9 @@
 # * Obiekt posiada dwa atrybuty `x` i `y` odpowiadające
 #   co do wartości parametrom przekazanym podczas instancjonowania
 # * W kodzie nie można użyć identyfikatora `self`
+
+class Point:
+    def __init__(foo, x, y):
+        foo.x = x
+        foo.y = y
+
