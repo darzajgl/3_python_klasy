c2580629675a220010d1896a5bfdb75b8357a253
Dariusz Zajglic <darzajgl@gmail.com>
2022-11-27 20:18:34 +0100

    task 2 done

--- task2.py
+++ task2.py
@@ -3,3 +3,8 @@
 #   instancjonowania
 # * Obiekt posiada dwa atrybuty `name` i `surname` odpowiadające
 #   co do wartości parametrom przekazanym podczas instancjonowania
+
+class Person():
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
