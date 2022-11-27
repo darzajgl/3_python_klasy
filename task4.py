f50e705bd357b9cabc715857f40d97ef0c189cc4
Dariusz Zajglic <darzajgl@gmail.com>
2022-11-27 21:01:07 +0100

    task 4 done

--- task4.py
+++ task4.py
@@ -3,3 +3,11 @@
 #   instancjonowania
 # * Obiekt posiada metodę `calculate` zwracającą rezultat
 #   dodawania argumentów podanych przy instancjonowaniu
+
+class Adder:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def calculate(self):
+        return self.x + self.y
